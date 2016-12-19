55a26440a3a770f77cfbe06f15ba7f1d320f7d82
He Zhang (Rudy) <425220727@qq.com>
2016-12-19 15:52:49 +0800

    Update 035_32.1_NativeStringMatcher.cpp

--- 035_32.1_NativeStringMatcher.cpp
+++ 035_32.1_NativeStringMatcher.cpp
@@ -24,3 +24,50 @@ void main() {
 
 
 }
+
+
+
+
+#include<iostream>
+#include<string>
+
+using std::string;
+
+//void nativeStringMatcher(string text, string pattern) {
+//	int n = text.length();
+//	int m = pattern.length();
+//
+//	 
+//
+//	for (int s = 0; s <= n-m+1; s++) {
+//		string temp = text.substr(s, m);
+//		if (pattern == temp) {
+//			std::cout << "在位置" << s << "处发现匹配" << std::endl; 
+//		}
+//	}
+//}
+
+void nativeStringMatcher(char *text, char *pattern, int n, int m) {
+
+	for (int s = 0; s <= n-m+1; s++) {
+
+		for (int i = 0; i < m; i++) {
+			if (text[s+i] != pattern[i]) {
+				break;
+			}
+			if(i == m-1) {
+			std::cout << "在位置" << s << "处发现匹配" << std::endl;
+			}
+		}
+	}
+}
+
+void main() {
+
+	/*string text("acaabcsdfgaabasd");
+	string pattern("aab");*
+	char *text = "acaabcsdfgaabasdsdfgaabasdsdfgaabasd";//16
+	char *pattern = "aab";//3
+
+	nativeStringMatcher(text, pattern, 37, 3);
+}
